2e7b4774151b8526f1e3210b1b520e5cff9d70fe
Carlos Arguelles <arguelles@wisc.edu>
2015-08-03 15:00:50 +0200

    fix bug

--- mains/test_crazy.cpp
+++ mains/test_crazy.cpp
@@ -8,8 +8,8 @@ using namespace nusquids;
 
 int main(){
   //std::cout << "DM" << std::endl;
+  std::shared_ptr<littlemermaid> sm = std::make_shared<littlemermaid>("Standard");
   std::shared_ptr<littlemermaid> dm = std::make_shared<littlemermaid>("DM");
   //std::cout << "SM" << std::endl;
-  std::shared_ptr<littlemermaid> sm = std::make_shared<littlemermaid>("Standard");
   return 0;
 }
